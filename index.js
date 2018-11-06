15a514ed37588aa385a96478afa47895608ffddd
Rafael A. R. Dias <eu@diasrafael.com.br>
2018-11-06 18:53:15 -0200

    correção

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const moment = require('moment')
 
-const normalizeIsoDate = (isoDate) => {
+const normalizeIsoDate = isoDate => {
     if ([ '0000-00-00', '00:00:00', '0000-00-00 00:00:00', ].indexOf(isoDate) === -1) {
-        return moment(isoDate)
+        return moment(isoDate).format('YYYY-MM-DD HH:mm:ss')
     }
 
     return null
